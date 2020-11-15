a9f6a92fddc395ad8f683f54d3c640a21d2ec7d5
Rae Stichter <raestichter@gmail.com>
2020-11-15 18:09:28 -0500

    edit and save the task text and task dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    //console.log(list, arr); lesson 5.1.5 said to comment
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,7 +45,107 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// ------------------------------------------ Edit task text ------------------------------------------
+// added in 5.1.6 to make it so every time p is clicked, a function runs
+// when the parapraph element of a task is clicked, it becomes editable.  Focus will appear around the element
+$(".list-group").on("click", "p", function() {
+  // console.log(this); // 'this' refers to the actual elements that are clicked on.  would show whatevver the <p> element test is
+  var text = $(this)
+    .text()
+    .trim();
+  
+  var textInput = $("<textarea>") // tells Jquery to find all existing <textarea> elements
+    .addClass("form-control")
+    .val(text);
+  $(this).replaceWith(textInput); // turns the clicked on area into a text input
+  textInput.trigger("focus"); // adds emphasis around the text area
+});
 
+// when you click off of the recently edited paragraph with the focus, the edited text will be saved.  Once focus is gone, save
+$(".list-group").on("blur", "textarea", function() { // blur event will trigger as soon as the user interacts with anything other that the text area element
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id") // this will return the ID, which will be "list-" followed by the category.  Then the replace() is run to update
+    .replace("list-", ""); // reguar js operator.  finds and replaces text in a string
+  
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index(); // index function from jquery indexes just like arrays, starting from 0
+  
+  // we want to update the overarching tasks object with the new data
+  tasks[status][index].text = text; // tasks is the object / tasks[status] returns an array (ie: toDo) / 
+  //console.log(tasks);
+  // tasks[status][index] returns the object at a given index in the array / tasks[status][index].text returns the text property
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+});
+
+// ------------------------------------------ Edit task date ------------------------------------------
+// very similar to editing the <p> element above
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 
 // modal was triggered
